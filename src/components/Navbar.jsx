9f4e07448b33f1311d0c8f88e1a30f7cede52eea
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-09-20 12:04:38 +0530

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar-wrapper'>
+      <nav className='navbar'>
+      <h1>Burger king</h1>
+      <button>get user</button>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
